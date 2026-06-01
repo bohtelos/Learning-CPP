61700e3185c6db4acadd3ff56d5cbbacc902aa79
bohtelos <bohtelos@gmail.com>
2026-06-01 13:32:54 +0300

    Add mark-as-done feature and continuous loop

--- todolist.cpp
+++ todolist.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <cctype>
 
 struct Task {
     std::string text;
@@ -9,6 +10,7 @@ struct Task {
 };
 
 int main() {
+
 
     std::cout << "A todo tool by bohtelos\n";
 
@@ -33,6 +35,10 @@ int main() {
         inFile.close();
     }
 
+    int taskNumber;
+    bool keepGoing = true;
+
+    while (keepGoing == true) {
     std::cout << "\n---- List of my tasks ----\n";
     for (size_t i = 0; i < todoList.size(); ++i) {
 
@@ -43,23 +49,59 @@ int main() {
             std::cout << "[] ";
         }
 
-        std::cout << todoList[i].text << "\n";
-
+        std::cout << i << ". " << todoList[i].text << "\n";
     }
 
-    std::cout << "\nDo you want to add new task? (Or press ENTER to leave): \n";
-    std::string newText;
-    std::getline(std::cin, newText);
+        std::cout << "\nEnter task number to mark as done or -1 to skip: ";
+        std::cin >> taskNumber;
+        std::cin.ignore();
 
-    if (!newText.empty()) {
-        Task newTask;
-        newTask.text = newText;
-        newTask.isDone = false;
+        if (taskNumber == -1) {
+            std::cout << "Skipping...\n";
+        }
 
-        todoList.push_back(newTask);
-        std::cout << "\nNew task has been added to the list.\n";
+        else if (taskNumber < 0 || taskNumber >= todoList.size()) {
+            std::cout << "\nWrong input!\n";
+        }
+        else {
 
+            todoList[taskNumber].isDone = true;
+            std::cout << "Task marked as done!\n";
+        }
+
+        std::cout << "\nDo you want to add new task? (Or press ENTER to leave): \n";
+        std::string newText;
+        std::getline(std::cin, newText);
+
+        if (!newText.empty()) {
+            Task newTask;
+            newTask.text = newText;
+            newTask.isDone = false;
+
+            todoList.push_back(newTask);
+            std::cout << "\nNew task has been added to the list.\n";
+
+        }
+
+        std::cout << "\nDo you want to continue? (y/n): ";
+        char answer;
+        std::cin >> answer;
+        answer = std::tolower(answer);
+
+        switch (answer) {
+            case 'y':
+                keepGoing = true;
+                break;
+            case 'n':
+                keepGoing = false;
+                break;
+            default:
+                std::cout << "\nWrong command.\n";
+                break;
+
+        }
     }
+
 
     std::ofstream outFile("todo.txt");
     if (outFile.is_open()) {
